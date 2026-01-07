d6e95a72db9c898e636179f0a7d5771d0425bb03
Nam <tintac_tk3@yahoo.com>
2026-01-07 22:18:49 +0700

    Update created at and updated at

--- Final_Project/database/models/student.py
+++ Final_Project/database/models/student.py
@@ -1,7 +1,18 @@
+from datetime import datetime, timezone
+
 from sqlmodel import Field, SQLModel
 
 
-class Student(SQLModel, table=True):
+class CreationTrackable(SQLModel):
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
+
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        nullable=False,
+        sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)},
+    )
+
+class Student(CreationTrackable, table=True):
     student_id: str | None = Field(default=None, primary_key=True)
     first_name: str = Field(index=False)
     last_name: str = Field(index=False)
